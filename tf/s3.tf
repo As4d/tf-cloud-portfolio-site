8bc2db1d80b00c59cf097c85672a04c779cc6e28
Asad <asadkhan91003@gmail.com>
2025-09-01 00:41:55 +0100

    Reverting Block public access (bucket settings)

--- tf/s3.tf
+++ tf/s3.tf
@@ -3,6 +3,16 @@ resource "aws_s3_bucket" "static_portfolio_site" {
   bucket = var.bucket_name
 }
 
+// block public access
+resource "aws_s3_bucket_public_access_block" "static_portfolio_site" {
+  bucket = aws_s3_bucket.static_portfolio_site.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 // build a bucket policy that allows cloudfront to read the bucket content only
 data "aws_iam_policy_document" "allow_cloudfront_service_principal_readonly" {
   statement {
