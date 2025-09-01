8172a0caf3e83a62d439bccaa93b8d9e824fec1a
Asad <asadkhan91003@gmail.com>
2025-09-01 02:30:01 +0100

    Fronting CloudFront with route53

---
+++ tf/route53.tf
@@ -0,0 +1,18 @@
+// Hosted zone
+resource "aws_route53_zone" "portfolio_site_hosted_zone" {
+  name = var.portfolio_site_domain_name
+}
+
+// Create record pointing to CloudFront
+resource "aws_route53_record" "portfolio_site" {
+  zone_id = aws_route53_zone.portfolio_site_hosted_zone.zone_id
+  name    = var.portfolio_site_domain_name
+  type    = "A"
+
+  alias {
+    name    = aws_cloudfront_distribution.static_portfolio_site_s3.domain_name
+    zone_id = aws_cloudfront_distribution.static_portfolio_site_s3.hosted_zone_id
+    // CloudFront is a managed service.
+    evaluate_target_health = false
+  }
+}
