3d875e4a96aa54a65320362698e43b5cb7c9fe7d
Asad <asadkhan91003@gmail.com>
2025-09-01 02:42:03 +0100

    Modify route53 to use existing hosted zone

--- tf/route53.tf
+++ tf/route53.tf
@@ -1,6 +1,7 @@
 // Hosted zone
-resource "aws_route53_zone" "portfolio_site_hosted_zone" {
-  name = var.portfolio_site_domain_name
+data "aws_route53_zone" "portfolio_site_hosted_zone" {
+  name         = var.portfolio_site_domain_name
+  private_zone = false
 }
 
 // Create record pointing to CloudFront
